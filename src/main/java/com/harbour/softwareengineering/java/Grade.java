9e0106d3100b24e93c330b69bc36dfa80f8f333e
aleksandrkokov <kokov.alexander@gmail.com>
2023-02-13 20:46:41 +0100

    files added

---
+++ src/main/java/com/harbour/softwareengineering/java/Grade.java
@@ -0,0 +1,4 @@
+package com.harbour.softwareengineering.java;
+
+public record Grade(int gr) {
+}
---
+++ src/main/java/com/harbour/softwareengineering/java/Person.java
@@ -0,0 +1,14 @@
+package com.harbour.softwareengineering.java;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public record Person(int id){
+//       Map<String, Integer> result;
+//       int id;
+
+}
