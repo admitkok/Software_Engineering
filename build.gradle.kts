d417f035c667c4143538b1cb424402ba90b73afb
aleksandrkokov <kokov.alexander@gmail.com>
2023-02-14 15:08:50 +0100

    update

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.0.2"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("maven-publish")
 }
 
 group = "com.harbour.softwareengineering"
@@ -9,7 +10,16 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
-	mavenCentral()
+	maven{
+		name = "GitHubPackages"
+		url = uri("https://github.com/admitkok/Software_Engineering")
+
+		credentials{
+			username = System.getenv("GITHUB_ACTOR")
+			password = System.getenv("GITHUB_TOKEN")
+		}
+	}
+
 }
 
 dependencies {
